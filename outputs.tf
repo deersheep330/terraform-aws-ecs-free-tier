0a8032afef5df139e95e1bd24752b6edbe29cc3c
deersheep330 <deersheep330@gmail.com>
2021-05-06 16:57:07 +0800

    output rds connection url

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "ecs_cluster_id" {
   description = "ecs cluster created from terraform-aws-ecs-free-tier project"
   value = aws_ecs_cluster.ecs_cluster.id
 }
+
+output "rds_connection_url" {
+  description = "rds connection url"
+  value = "${aws_db_instance.rds.username}:${aws_db_instance.rds.password}@${aws_db_instance.rds.endpoint}/${aws_db_instance.rds.name}"
+}
